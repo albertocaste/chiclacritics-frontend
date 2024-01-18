d04fb96f643aa12255e00fdc300f465a691947eb
Alberto Castellanos Martínez <caste_acm@hotmail.com>
2024-01-18 01:27:19 +0100

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { GameComponent } from './ui/components/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'games',
+        component: GameComponent,
+        data: {
+            title: 'games',
+            breadcrumb: 'games'
+        }
+    }
+];
